1a626eb9ee6a30750d50c1ade0dbb1463bcbc36f
emelecio966 <143520582+emelecio966@users.noreply.github.com>
2024-04-24 14:38:23 +0000

    roll dice

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get("dice/2/5", { :controller => "dice", :action => "two6" })
   
-  get("/my_cool_route/:param_one/:param_two", { :controller => "my_controller", :action => "some_action" })
+  get ("dice/2/10", { :controller => "dice", :action => " two10" })
+
+  get ("dice/1/20", { :controller => "dice", :action => "one20" }) 
+
+  get ("dice/5/4", { :controller => "dice", :action => "five4"})
   
+  get ("dice/:number/:sides", { :controller => "dice", :action => "dynamic" })
 end
