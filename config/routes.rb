b3576f03fbe4457ed7076a089dbc5dbade1f6b57
emelecio966 <143520582+emelecio966@users.noreply.github.com>
2024-04-26 14:09:25 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get("dice/2/5", { :controller => "dice", :action => "two6" })
+  get("dice/2/6", { :controller => "dice", :action => "two6" })
   
-  get ("dice/2/10", { :controller => "dice", :action => " two10" })
+  get ("dice/2/10", { :controller => "dice", :action => "two10" })
 
-  get ("dice/1/20", { :controller => "dice", :action => "one20" }) 
+  get ("dice/1/20", { :controller => "dice", :action => "one20" })
 
   get ("dice/5/4", { :controller => "dice", :action => "five4"})
   
   get ("dice/:number/:sides", { :controller => "dice", :action => "dynamic" })
+
+  get("/", { :controller => "dice", :action => "home"})
 end
